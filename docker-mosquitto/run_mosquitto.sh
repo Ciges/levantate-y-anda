5f06f61c4959839a9074031aa9d204ed6b261a88
Ciges <jmanuel@ciges.net>
2021-02-20 16:43:33 +0000

    La configuración de mosquitto en local es montada en el contenedor

--- docker-mosquitto/run_mosquitto.sh
+++ docker-mosquitto/run_mosquitto.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #docker pull eclipse-mosquitto
-docker run --name mosquitto -it --rm --network host eclipse-mosquitto
+docker run --name mosquitto -it -d --network host --mount type=bind,source="$PWD/mosquitto",target=/mosquitto eclipse-mosquitto
